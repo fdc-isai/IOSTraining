b1cb8f3617b0969147158c7f4ed3376e82ed9bf5
fdc-isai <48667278+fdc-isai@users.noreply.github.com>
2025-07-07 10:05:44 +0800

    Initial Commit

---
+++ IOSTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSTraining
+//
+//  Created by Isai Gesapine on 7/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
